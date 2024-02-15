ed2009d091840802237737c45899f328b24d541f
surendraseepala <seepalasurendra@gmail.com>
2024-02-15 23:38:04 +0530

    first commit

---
+++ quizwuiz/questions.js
@@ -0,0 +1,72 @@
+var questionBank = [
+    {
+        question: "What does HTML stand for?",
+        option: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Transfer Protocol",
+            "Hyper Text Markup Language",
+            "Hyper Tool Multi Language"
+        ],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "What tag is used to define an unordered list in HTML?",
+        option: [
+            "ul",
+            "ol",
+            "li",
+            "list"
+        ],
+        answer: "ul"
+    },
+    {
+        question: "Which HTML attribute is used to define inline styles?",
+        option: [
+            "style",
+            "class",
+            "id",
+            "font"
+        ],
+        answer: "style"
+    },
+    {
+        question: "What does the img tag do in HTML?",
+        option: [
+            "Defines a hyperlink",
+            "Embeds an image",
+            "Defines a table",
+            "Defines a form"
+        ],
+        answer: "Embeds an image"
+    },
+    {
+        question: "What is the correct HTML element for inserting a line break?",
+        option: [
+            "br",
+            "lb",
+            "break",
+            "ln"
+        ],
+        answer: "br"
+    },
+    {
+        question: "What does CSS stand for?",
+        option: [
+            "Cascading Style Sheets",
+            "Creative Style Syntax",
+            "Computer Style Systems",
+            "Colorful Styling Scripts"
+        ],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: "Which property is used to change the background color of an element in CSS?",
+        option: [
+            "color",
+            "background-color",
+            "text-color",
+            "bg-color"
+        ],
+        answer: "background-color"
+    }
+]
--- quizwuiz/quiz.js
+++ quizwuiz/quiz.js
@@ -1,31 +1,3 @@
-//Question bank
-var questionBank= [
-    {
-        question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
-        option : ['Asia','Africa','Europe','Australia'],
-        answer : 'Africa'
-    },
-    {
-        question : 'Garampani sanctuary is located at',
-        option : ['Junagarh, Gujarat','Diphu, Assam','Kohima, Nagaland','Gangtok, Sikkim'],
-        answer : 'Diphu, Assam'
-    },
-    {
-        question : 'For which of the following disciplines is Nobel Prize awarded?',
-        option : ['Physics and Chemistry','Physiology or Medicine','Literature, Peace and Economics','All of the above'],
-        answer : 'All of the above'
-    },
-    {
-        question : 'Hitler party which came into power in 1933 is known as',
-        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
-        answer : 'Nazi Party'
-    },
-    {
-        question : 'First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in',
-        option : ['1967','1968','1958','1922'],
-        answer : '1967'
-    }
-]
 
 var question= document.getElementById('question');
 var quizContainer= document.getElementById('quiz-container');
